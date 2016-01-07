3868f04f3e61f30dcaabc2deec8b4994e7bd54eb
radicalonion <ndefilippis98@gmail.com>
2016-01-06 19:05:54 -0500

    Update scaffold to 0.0.3 and classify robots

---
+++ src/team184/ScoutRobot.java
@@ -0,0 +1,15 @@
+package team184;
+
+import battlecode.common.RobotController;
+
+public class ScoutRobot extends BaseRobot {
+
+	public ScoutRobot(RobotController rc){
+		super(rc);
+	}
+
+	@Override
+	public void run() {
+		defaultBehavior();
+	}
+}
---
+++ src/team184/SoldierRobot.java
@@ -0,0 +1,15 @@
+package team184;
+
+import battlecode.common.RobotController;
+
+public class SoldierRobot  extends BaseRobot {
+	
+	public SoldierRobot(RobotController rc){
+		super(rc);
+	}
+
+	@Override
+	public void run() {
+		defaultBehavior();
+	}
+}
